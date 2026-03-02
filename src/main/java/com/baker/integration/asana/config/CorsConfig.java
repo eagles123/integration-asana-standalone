5ba265ab70c0918dee42f1632138e69287b08d8f
KevinFeng <kevin.feng@lytho.com>
2026-03-02 16:25:44 +0100

    add cors

---
+++ src/main/java/com/baker/integration/asana/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.baker.integration.asana.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("https://app.asana.com"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/asana/**", config);
+        return new CorsFilter(source);
+    }
+}
